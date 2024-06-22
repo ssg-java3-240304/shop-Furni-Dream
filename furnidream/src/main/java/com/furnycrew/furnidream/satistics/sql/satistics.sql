600de6cd688096f005e8a9a78d75fc5ea49d8f58
Hongjimin <jmh907@naver.com>
2024-06-23 08:15:48 +0900

    Feat : 매출조회 쿼리문 작성

---
+++ furnidream/src/main/java/com/furnycrew/furnidream/satistics/sql/satistics.sql
@@ -0,0 +1,81 @@
+select * from tbl_order order by created_at;
+select * from tbl_order;
+select * from tbl_order_product;
+select * from tbl_order_canceled;
+select * from tbl_product;
+
+select
+    *
+from tbl_order o
+         left join tbl_order_product op
+                   using (order_code)
+         left join tbl_order_canceled using (order_code);
+
+-- 일자별 매출 합계
+select
+    date_format(o.created_at, '%Y-%m-%d') 주문일자,
+    sum(op.quantity) 주문건수,
+    sum(op.quantity * op.price) 결제금액,
+    ifnull(sum(oc.refund_amount), 0) 환불합계,
+    sum(op.quantity * op.price) - ifnull(sum(oc.refund_amount), 0) 매출합계
+from tbl_order o
+         left join tbl_order_product op
+                   using (order_code)
+         left join tbl_order_canceled oc using (order_code)
+group by
+    date_format(created_at, '%Y-%m-%d')
+order by 주문일자;
+
+-- 월별 매출 합계
+select
+    date_format(o.created_at, '%Y-%m') 주문일자,
+    sum(op.quantity) 주문건수,
+    sum(op.quantity * op.price) 결제금액,
+    ifnull(sum(oc.refund_amount), 0) 환불합계,
+    sum(op.quantity * op.price) - ifnull(sum(oc.refund_amount), 0) 매출합계
+
+from tbl_order o
+         left join tbl_order_product op
+                   using (order_code)
+         left join tbl_order_canceled oc using (order_code)
+group by
+    date_format(created_at, '%Y-%m')
+order by 주문일자;
+
+-- 계절별 💖아직 안함💖
+select
+    quarter(o.created_at) 주문일자,
+    sum(op.quantity) 주문건수,
+    sum(op.quantity * op.price) 결제금액,
+    ifnull(sum(oc.refund_amount), 0) 환불합계,
+    sum(op.quantity * op.price) - ifnull(sum(oc.refund_amount), 0) 매출합계
+
+from tbl_order o
+         left join tbl_order_product op
+                   using (order_code)
+         left join tbl_order_canceled oc using (order_code)
+group by
+    quarter(created_at)
+order by 주문일자;
+
+
+-- 상품별 매출 내역
+select
+    p.product_name,
+    sum(op.quantity) 판매수량,
+    sum(o.total_price)
+
+from tbl_order o
+         left join tbl_order_canceled oc
+                   using (order_code)
+         left join tbl_order_product op
+                   using (order_code)
+         left join tbl_product p
+                   using (product_id)
+group by
+    product_id
+order by
+    판매수량 desc ;
+
+-- 연령대별
+-- 성별
