4fd4e0aee67f86f9dc22e1072e761b549f433f6e
jeonhyeonseon <good_pretty@naver.com>
2024-06-23 00:52:08 +0900

    Update: 상품리스트 전체조회 view단 제외 구현 #3

--- furnidream/src/main/java/com/furnycrew/furnidream/product/controller/ProductController.java
+++ furnidream/src/main/java/com/furnycrew/furnidream/product/controller/ProductController.java
@@ -1,4 +1,28 @@
 package com.furnycrew.furnidream.product.controller;
 
+import com.furnycrew.furnidream.product.model.dto.ProductDto;
+import com.furnycrew.furnidream.product.model.service.ProductQueryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequestMapping("/product")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductQueryService productQueryService;
+
+    @GetMapping("/list")
+    public void list(Model model){
+        log.info("GET /product/list");
+        List<ProductDto> products = productQueryService.findAll();
+        log.debug("products = {}", products);
+//        model.addAllAttributes("products", products);
+    }
 }
