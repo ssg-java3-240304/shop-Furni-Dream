ce2c04f3dc8eda955e55ed10b58827036755b2f5
jeonhyeonseon <good_pretty@naver.com>
2024-06-23 18:50:22 +0900

    Update: 상품 등록 view단 제외 구현 #1

---
+++ furnidream/src/main/java/com/furnycrew/furnidream/product/model/dto/ProductRegistDto.java
@@ -0,0 +1,30 @@
+package com.furnycrew.furnidream.product.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRegistDto {
+    private String productName;
+    private String productImage;
+    private String category;
+    private int costPrice;
+    private int retailPrice;
+    private int stock;
+    private String description;
+    private String productCode;
+    private int discountRate;
+    private int shippingFee;
+    private ProductStatus productStatus;
+    private String color;
+    private int size;
+
+    public ProductDto toProductDto() {
+        return new ProductDto(null, this.productName, this.productImage, this.category, this.costPrice, this.retailPrice,
+                this.stock, this.description, this.productCode, this.discountRate, this.shippingFee, this.productStatus,
+                this.color, this.size);
+    }
+}
