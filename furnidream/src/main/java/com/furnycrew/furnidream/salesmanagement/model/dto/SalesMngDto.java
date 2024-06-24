857e18d084a44ee551e0739f7855619612c1803d
Hong-ji-min <jmh907@naver.com>
2024-06-24 20:03:41 +0900

    Feat : salesMng-mapper 수정 #5

--- furnidream/src/main/java/com/furnycrew/furnidream/salesmanagement/model/dto/SalesMngDto.java
+++ furnidream/src/main/java/com/furnycrew/furnidream/salesmanagement/model/dto/SalesMngDto.java
@@ -1,4 +1,11 @@
 package com.furnycrew.furnidream.salesmanagement.model.dto;
 
+import com.furnycrew.furnidream.order.model.dto.OrderCanceledDto;
+import com.furnycrew.furnidream.order.model.dto.OrderDto;
+import com.furnycrew.furnidream.order.model.dto.OrderProductDto;
+
 public class SalesMngDto {
+    private OrderDto orderDto;
+    private OrderProductDto productDto;
+    private OrderCanceledDto orderCanceledDto;
 }
